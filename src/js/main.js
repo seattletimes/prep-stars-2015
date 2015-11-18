1a5610c9df79e5f29baaff1ba1a8409c06dab397
Thomas Wilburn <news@thomaswilburn.net>
2015-11-18 15:24:43 -0800

    The basics

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,27 @@ require("./lib/ads");
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
+
+if ("ontouchstart" in window) {
+  document.body.classList.add("touch-enabled");
+}
+
+var $ = require("jquery");
+var dot = require("./lib/dot");
+var detailTemplate = dot.compile(require("./_detail.html"));
+
+var students = {};
+studentData.forEach(s => students[s.name] = s);
+
+var $slider = $(".slider");
+var $details = $(".details");
+
+$slider.on("click", ".student img", function() {
+  var $this = $(this);
+  $slider.find(".activated").removeClass("activated");
+  $this.closest(".student").addClass("activated");
+  var name = this.getAttribute("data-student");
+  var student = students[name];
+  $details.html(detailTemplate(student));
+  $details.addClass("has-content");
+});
